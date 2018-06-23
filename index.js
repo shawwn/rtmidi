a2220cc45053e2ffba57256fbed7cc856c54ec35
Shawn Presser <shawnpresser@gmail.com>
2018-06-23 16:38:27 -0500

    Initial midiprobe

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-Object.assign(module.exports, require('./build/Release/rtmidi.node'));
+Object.assign(module.exports, require(`./build/${process.env.RTMIDI_DEBUG ? 'Debug' : 'Release'}/rtmidi.node`));
--- main.cpp
+++ main.cpp
@@ -12,9 +12,69 @@
 #include <set>
 #include <nan.h>
 
+// midiprobe.cpp
+#include <iostream>
+#include <cstdlib>
+#include "RtMidi.h"
+int midiprobe()
+{
+  RtMidiIn  *midiin = 0;
+  RtMidiOut *midiout = 0;
+  // RtMidiIn constructor
+  try {
+    midiin = new RtMidiIn();
+  }
+  catch ( RtMidiError &error ) {
+    error.printMessage();
+    exit( EXIT_FAILURE );
+  }
+  // Check inputs.
+  unsigned int nPorts = midiin->getPortCount();
+  std::cout << "\nThere are " << nPorts << " MIDI input sources available.\n";
+  std::string portName;
+  for ( unsigned int i=0; i<nPorts; i++ ) {
+    try {
+      portName = midiin->getPortName(i);
+    }
+    catch ( RtMidiError &error ) {
+      error.printMessage();
+      goto cleanup;
+    }
+    std::cout << "  Input Port #" << i+1 << ": " << portName << '\n';
+  }
+  // RtMidiOut constructor
+  try {
+    midiout = new RtMidiOut();
+  }
+  catch ( RtMidiError &error ) {
+    error.printMessage();
+    exit( EXIT_FAILURE );
+  }
+  // Check outputs.
+  nPorts = midiout->getPortCount();
+  std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
+  for ( unsigned int i=0; i<nPorts; i++ ) {
+    try {
+      portName = midiout->getPortName(i);
+    }
+    catch (RtMidiError &error) {
+      error.printMessage();
+      goto cleanup;
+    }
+    std::cout << "  Output Port #" << i+1 << ": " << portName << '\n';
+  }
+  std::cout << '\n';
+  // Clean up
+ cleanup:
+  delete midiin;
+  delete midiout;
+  return 0;
+}
+
 // module
 
 extern "C" NAN_MODULE_INIT(init) {
+  midiprobe();
 }
 
 NODE_MODULE(rtmidi, init)
